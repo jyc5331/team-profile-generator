e18cd9d7c4bec3ffd925626e2fccc848f1a327eb
jyc5331 <jcosgrovecoding@gmail.com>
2021-01-16 17:18:22 -0500

    prompts complete and validations functional minus email

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const Employee = require("./lib/Employee");
-const emailValidator = require("email-validator");
+const validator = require("email-validator");
 
 const promptUserBasic = () => {
   return inquirer.prompt([
@@ -23,7 +23,7 @@ const promptUserBasic = () => {
       name: "id",
       message: "What is your ID number? (Required)",
       validate: (idInput) => {
-        if (idInput === Number) {
+        if ((idInput = Number)) {
           return true;
         } else {
           console.log("Please enter a valid number");
@@ -35,30 +35,109 @@ const promptUserBasic = () => {
       type: "input",
       name: "email",
       message: "What is your email address? (Required)",
-      //     validate: emailValidator (emailInput) => {
-      //       if (emailInput) {
-      //           return true
-      //       } else {
-      //           console.log ("Please enter a valid email address")
-      //           return false;
-      //       }
-      //   }
+      validate: (emailInput) => {
+        //HELP this isn't actually working
+        validator.validate(emailInput);
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter a valid email address");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "role",
+      message:
+        "What is your role? Please enter one of the following: Manager, Engineer, or Intern(Required)",
+      validate: (roleInput) => {
+        if (roleInput === "Manager" || "Engineer" || "Intern") {
+          //place the function that runs the other function here
+          return true;
+        } else {
+          console.log("Please enter your name");
+          return false;
+        }
+      },
+    },
+    //run an inquirer here asking if they want to add another employee, if true loop, if false run generateHTML
+    //either way (T/F) call a function that creates an object for the employee using the appopriate class, employee + i
+  ]);
+};
+
+const promptUserManager = (managerAnswers) => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "office",
+      message: "What is your office number? (Required)",
+      validate: (officeInput) => {
+        if ((officeInput = Number)) {
+          return true;
+        } else {
+          console.log("Please enter your office number");
+          return false;
+        }
+      },
     },
   ]);
 };
 
-const Bob = new Employee("Bob", 1, "example1@gmail.com");
-const Karen = new Employee("Karen", 2, "example2@gmail.com");
-const Jan = new Employee("Jan", 3, "example3@gmail.com");
+const promptUserEngineer = (engineerAnswers) => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "office",
+      message: "What is your github username? (Required)",
+      validate: (githubInput) => {
+        if (githubInput != "") {
+          return true;
+        } else {
+          console.log("Please enter your github username");
+          return false;
+        }
+      },
+    },
+  ]);
+};
 
-const employees = [Bob, Karen, Jan];
-let html = "";
+const promptUserIntern = (internAnswers) => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "school",
+      message: "What is your the name of your school? (Required)",
+      validate: (schoolInput) => {
+        if (schoolInput != "") {
+          return true;
+        } else {
+          console.log("Please enter your school name");
+          return false;
+        }
+      },
+    },
+  ]);
+};
+// const Bob = new Employee("Bob", 1, "example1@gmail.com");
+// const Karen = new Employee("Karen", 2, "example2@gmail.com");
+// const Jan = new Employee("Jan", 3, "example3@gmail.com");
 
-for (let i = 0; i < employees.length; i++) {
-  html += `Name: ${employees[i].getName()}
-Role: ${employees[i].getRole()}
-ID: ${employees[i].getId()}
-Email: ${employees[i].getEmail()}
-`;
-}
-console.log(html);
+// const employees = [Bob, Karen, Jan];
+// let html = "";
+
+// for (let i = 0; i < employees.length; i++) {
+//   html += `Name: ${employees[i].getName()}
+// Role: ${employees[i].getRole()}
+// ID: ${employees[i].getId()}
+// Email: ${employees[i].getEmail()}
+// `;
+// }
+// console.log(html);
+
+promptUserBasic().then((answers) => {
+  console.log(answers);
+  // fs.writeFile("newreadme.md", genmd(answers), (err) => {
+  //   if (err) throw err;
+  // });
+});
